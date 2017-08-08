d918e182a6b6d8774fc3915f887a3934e567e1c3
Yohann Atticot <yatticot@keley-live.com>
2017-08-08 06:33:09 +0200

    preparation du formulaire coté vue et de reactive form coté .ts

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-signup',
     templateUrl: 'signup.component.html',
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+    myForm: FormGroup;
+
+    ngOnInit(){
+        this.myForm = new FormGroup({
+            firstName: new FormControl(null, Validators.required),
+            lasttName: new FormControl(null, Validators.required),
+            // Pour ajouter plusieurs controle de validation, il suffit de les ajouter dans un array
+            email: new FormControl(null, [
+                Validators.required,
+                // RegExp pour une adresse mail valide
+                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9]*[a-z0-9])?")
+                ]),
+            password: new FormControl(null, Validators.required)
+        }); 
+    }
 }
