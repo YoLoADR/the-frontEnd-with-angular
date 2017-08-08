bf35f8198fbd498e7904001c569fa3622d849554
Yohann Atticot <yatticot@keley-live.com>
2017-08-08 16:57:04 +0200

    enregistrer des données (message) coté back

---
+++ routes/messages.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+// On a besoin d'avoir accès à notre message model pour pouvoir intéragir avec lui (sauvegarder dans la base ect ...)
+var Message = require('../models/message');
+
+//"post" -> parce que on veut stocker un message grace au server
+router.post('/', function (req, res, next) {
+    var message = new Message({
+      content : req.body.content
+    });
+    // Pour sauvegarder / sotcker le message dans la base de données
+    // Avec un callback pour verifier qu'on n'est pas une erreur et recuperer le resultat de cette opération
+    message.save(function(err, result){
+      //Dans le cas où on a une erreur
+      if(err){
+        return res.status(500).json({
+          title: 'Une erreur à été detecter',
+          error: err
+        });
+      }
+      // status 201 Everythings it's ok
+      res.status(201).json({
+        message: 'message enregistrer',
+        obj : result
+      })
+
+    });
+});
+
+module.exports = router;
